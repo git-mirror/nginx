git-mirror/nginx

commit 6ac3aa08bfc54e59dcdc436fa4992616fdd70875

$geoip_region_name
git-svn-id: svn://svn.nginx.org/nginx/trunk@3738 c3fe7df1-7212-e011-8a91-001109144009

src/http/modules/ngx_http_geoip_module.c
@@ -30,6 +30,8 @@ static ngx_int_t ngx_http_geoip_country_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
 static ngx_int_t ngx_http_geoip_city_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
+static ngx_int_t ngx_http_geoip_region_name_variable(ngx_http_request_t *r,
+    ngx_http_variable_value_t *v, uintptr_t data);
 static ngx_int_t ngx_http_geoip_city_float_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
 static ngx_int_t ngx_http_geoip_city_int_variable(ngx_http_request_t *r,
@@ -129,6 +131,10 @@ static ngx_http_variable_t  ngx_http_geoip_vars[] = {
     { ngx_string("geoip_region"), NULL,
       ngx_http_geoip_city_variable,
       offsetof(GeoIPRecord, region), 0, 0 },
+
+    { ngx_string("geoip_region_name"), NULL,
+      ngx_http_geoip_region_name_variable,
+      0, 0, 0 },
 
     { ngx_string("geoip_city"), NULL,
       ngx_http_geoip_city_variable,
@@ -245,6 +251,48 @@ ngx_http_geoip_city_variable(ngx_http_request_t *r,
 no_value:
 
     GeoIPRecord_delete(gr);
+
+not_found:
+
+    v->not_found = 1;
+
+    return NGX_OK;
+}
+
+
+static ngx_int_t
+ngx_http_geoip_region_name_variable(ngx_http_request_t *r,
+    ngx_http_variable_value_t *v, uintptr_t data)
+{
+    size_t        len;
+    const char   *val;
+    GeoIPRecord  *gr;
+
+    gr = ngx_http_geoip_get_city_record(r);
+    if (gr == NULL) {
+        goto not_found;
+    }
+
+    val = GeoIP_region_name_by_code(gr->country_code, gr->region);
+
+    len = ngx_strlen(val);
+    v->data = ngx_pnalloc(r->pool, len);
+
+    if (v->data == NULL) {
+        GeoIPRecord_delete(gr);
+        return NGX_ERROR;
+    }
+
+    ngx_memcpy(v->data, val, len);
+
+    v->len = len;
+    v->valid = 1;
+    v->no_cacheable = 0;
+    v->not_found = 0;
+
+    GeoIPRecord_delete(gr);
+
+    return NGX_OK;
 
 not_found:
 

commit 008e169e2325459ef2a162adfa1ce5128c6813b3

fix segfault, the bug has been introduced in r3738

src/http/modules/ngx_http_geoip_module.c
@@ -274,6 +274,9 @@ ngx_http_geoip_region_name_variable(ngx_http_request_t *r,
     }
 
     val = GeoIP_region_name_by_code(gr->country_code, gr->region);
+    if (val == NULL) {
+        goto no_value;
+    }
 
     len = ngx_strlen(val);
     v->data = ngx_pnalloc(r->pool, len);
@@ -293,6 +296,10 @@ ngx_http_geoip_region_name_variable(ngx_http_request_t *r,
     GeoIPRecord_delete(gr);
 
     return NGX_OK;
+
+no_value:
+
+    GeoIPRecord_delete(gr);
 
 not_found:
 